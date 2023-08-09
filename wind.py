e15ac2e2431108844b902b4e40819d772fed2899
asidsx <106923482+asidsx@users.noreply.github.com>
2023-08-09 15:19:00 +0500

    Update wind.py
    The file sorting has been revised, and the skipping of lines containing double quotes has been fixed.
    Была переделана сортировка файлов, и устранен пропуск строк в которых были двойные кавычки.

--- wind.py
+++ wind.py
@@ -29,6 +29,8 @@ class Application(tk.Frame):
         self.ch_button.pack(fill=tk.X, pady=10)
         
 
+
+
     def load_eng(self):
         global index
         index = 0
@@ -47,7 +49,8 @@ class Application(tk.Frame):
     def load_TW(self):
         global index
         index = 3
-        self.load_lang()  
+        self.load_lang()
+     
         
     def load_lang(self):
         with open("input.txt", "r", encoding="utf-8") as file:
@@ -73,7 +76,7 @@ class Application(tk.Frame):
         input_file = 'output.txt'
         output_file = 'output2.txt'
         term_regex = re.compile(r'Term = "(.+?)"')
-        language_regex = re.compile(rf'\[{index}\]1 string Languages = "((?:<.*?>|\s|[^"])+)"')
+        language_regex = re.compile(rf'\[{index}\]1 string Languages = "((?:<.*?>|\s|[^"]*(?:"[^"]*"[^"]*)*[^"]*)+)"')
         terms = {}
         with open(input_file, 'r', encoding='utf-8') as f_input:
             for line in f_input:
@@ -109,21 +112,88 @@ class Application(tk.Frame):
                     line = line[:start_index] + content + line[end_index+1:]
                     start_index = end_index
                 output_file.write(line)
-        def fix_json_file(file_path):
+
+
+
+
+
+
+
+        def process_line(line, last_line=False):
+            
+            parts = line.split(': "', 1)
+            if len(parts) == 2:
+                key = parts[0]
+                value = parts[1].rsplit('",', 1)[0]
+                
+                # Игнорируем обработку, если в значении встречается \"
+                if '\\"' in value:
+                    return line
+                
+                # Обработка значения, как и ранее
+                value = value.replace('"', '\\"')
+                return f'{key}: "{value}",' if not last_line else f'{key}: "{value}"'
+            
+            return line
+
+        input_filename = 'output2.txt'
+        output_filename = 'output2.txt'
+
+        with open(input_filename, 'r', encoding='utf-8') as input_file:
+            lines = input_file.readlines()
+
+        # Добавляем { в начало и } на новую строку в конце
+        processed_lines = ['{'] + [process_line(line) for line in lines] + ['}']
+
+        with open(output_filename, 'w', encoding='utf-8') as output_file:
+            output_file.write('\n'.join(processed_lines))
+
+
+
+
+        def fix_json_file(input_file, output_file):
+            try:
+                with open(input_file, 'r', encoding='utf-8') as f:
+                    content = f.read()
+
+                    try:
+                        json_data = json.loads(content)
+                    except json.JSONDecodeError:
+                        content = '{' + content + '}'
+                        json_data = json.loads(content)
+                    
+                    with open(output_file, 'w', encoding='utf-8') as out_f:
+                        json.dump(json_data, out_f, indent=4, ensure_ascii=False)
+
+                    print("Файл успешно исправлен и сохранен.")
+            except Exception as e:
+                print(f"Произошла ошибка: {e}")
+
+
+
+        # Пример вызова функции
+        fix_json_file('output2.txt', 'output2.txt')
+
+        def fix_json2_file(file_path):
             with open(file_path, 'r', encoding='utf-8') as f:
                 lines = f.readlines()
-            if len(lines) > 0:
-                if lines[0][0] != '{':
-                    lines.insert(0, '{\n')
-                if lines[-1][-2:] == ',\n':
-                    lines[-1] = lines[-1][:-2] + '\n'
-                elif lines[-1][-1] == '\n':
-                    lines[-1] = lines[-1][:-1]
-            with open(file_path, 'w', encoding='utf-8') as f:
-                f.writelines(lines)
-                if lines[-1][-1] != '}':
-                    f.write('}\n')
-        fix_json_file('output2.txt')
+
+            if lines:
+                joined_lines = ''.join(lines)
+                # Ищем последнюю запятую перед фигурной скобкой и удаляем её
+                updated_content = joined_lines.rsplit(',', 1)[0] + '\n}'
+
+                # Удаление пустых строк
+                updated_content = '\n'.join([line for line in updated_content.split('\n') if line.strip()])
+
+                with open(file_path, 'w', encoding='utf-8') as f:
+                    f.write(updated_content)
+
+        # Пример использования
+        fix_json2_file('output2.txt')
+
+            
+        # Остальшой код остается неизменным
         with open('output2.txt', encoding='utf-8') as f1, open('old.txt', encoding='utf-8') as f2:
             file1_data = json.load(f1)
             file2_data = json.load(f2)
@@ -140,9 +210,10 @@ class Application(tk.Frame):
         old_name = 'output2.txt'
         new_name = 'orig.txt'
         if os.path.exists(new_name):
-            os.remove(new_name)  # удалить существующий файл
+            os.remove(new_name)
         os.rename(old_name, new_name)
-        
+
+
 root = tk.Tk()
 root.title("EasyTranslateCraftopia")
 app = Application(master=root)
